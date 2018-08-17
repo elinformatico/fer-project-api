3cb49ed8f1339252fc8025dbfc39b8c1cbab3291
elinformatico <donoe1985@gmail.com>
2018-08-16 20:30:41 -0500

    fix: Se corrigio el fullName para que obtenga el nombre completo en el Token

--- app/Http/Controllers/Usuario.php
+++ app/Http/Controllers/Usuario.php
@@ -12,28 +12,28 @@ class Usuario extends Controller
 {
     public function getUsuarios() 
     {
-    	try {
+        try {
             $usuarios = DB::table('usuario')->get();
-	    	return Response()->json(
-	    		array(
+            return Response()->json(
+                array(
                     'msg'=>'Vientos, todo jala chido', 
                     'usuarios' => $usuarios, 
                     "token" => "ZWxpbmZvcm1hdGljbw==|dXNlcg==|MjAxOC0wOC0wNCAxOjUyOjM5"
                 )
-	    	);
+            );
         } catch(\Illuminate\Database\QueryException $e){
             return Response()->json(
-            	array('msg'=>'Error de query al consultar los datos.','error'=>$e)
+                array('msg'=>'Error de query al consultar los datos.','error'=>$e)
             );
         }
     }
 
-    public function generateToken($username, $typeUser, $minutesToExpire) 
+    public function generateToken($username, $fullName, $typeUser, $minutesToExpire) 
     {
         $dateExpired = date("Y-m-d G:i:s", 
         mktime(date("G"), date("i") + $minutesToExpire, date("s"), date("m"), date("d"), date("Y"))); 
 
-        $token = base64_encode($username) . "|" . base64_encode($typeUser) . "|" . base64_encode($dateExpired);
+        $token = base64_encode($username) . "|" . base64_encode($fullName) . "|" . base64_encode($typeUser) . "|" . base64_encode($dateExpired);
         return $token;
     }
 
@@ -43,20 +43,24 @@ class Usuario extends Controller
             $user = DB::table('usuario')
                             ->select(
                                 "usr_nombre_usuario as username",
-                                "usr_nombre_completo as fullName",
+                                "usr_nombres as nombre",
+                                "usr_apellido_paterno as paterno",
+                                "usr_apellido_materno as materno",
                                 "usr_tipo as typeUser"
                             )
                             ->where("usr_nombre_usuario", $username)
                             ->where("usr_password", $password)
                             ->first();
 
-            if(isset($user->username)){
-                $token = $this->generateToken($user->username, $user->typeUser, 30);
+            if(isset($user->username))
+            {
+                $fullName = "{$user->nombre} {$user->paterno} {$user->materno}";
+
+                $token = $this->generateToken($user->username, $fullName, $user->typeUser, 30);
 
                 return Response()->json(
                     array(
-                        'msg'       =>'Los datos de usuario se obtuvieron satisfactoriamente', 
-                        'fullName'  => $user->fullName, 
+                        'msg'       =>'Los datos de usuario se obtuvieron satisfactoriamente',            
                         "token"     => $token,
                         "status"    => "success",
                     )
@@ -80,6 +84,6 @@ class Usuario extends Controller
 
     public function consumeApi() 
     {
-    	
+        
     }
 }
