65bf9c9a820d95ff1dd5e19c5d6d25cd098c7c17
J.Noe Hernandez Vivanco <donoe1985@gmail.com>
2018-11-22 16:54:46 -0500

    fix: Se arreglo el servicio que trae los datos de la corresponencia para la busqueda

--- app/Http/Controllers/Consultas.php
+++ app/Http/Controllers/Consultas.php
@@ -13,7 +13,6 @@ class Consultas extends Controller
     public function getCorrespondencia() 
     {
         try {
-         
             $correspondencias = DB::table('correspondencia')
                 ->select(
                     "cor_id as id",
@@ -56,14 +55,96 @@ class Consultas extends Controller
                 })
                 //->toSql();
                 ->get();
+          
+                $filterCorrespondencia = [];
+                foreach($correspondencias as $value) 
+                {
+                    $strFechaLimite = "";
+                    $colorStatus = "cor_green";
+                    
+                     # Formato: YYYY-MM-DD
+                     $fechaLimiteDB = substr($value->fecha_limite, 0, 10);
+                    
+                    if($value->fecha_limite == "1990-01-01 00:00:00") {
+                        $strFechaLimite = "Abierto";
+                        $fechaLimiteDB = "_";
+                    } else {
+                        
+                        # http://php.net/manual/es/function.date-diff.php
+                        $databaseDate = date_create(substr($value->fecha_limite, 0, 10));
+                        $currentData  = date_create(date("Y-m-d")); # Example: "2018-11-05 23:59:59" --> date("Y-m-d")
+                        $diffDays = date_diff($databaseDate, $currentData, true);
+                        
+                        # Hoy > Fecha_Limite
+                        if(strtotime(date("Y-m-d")) > strtotime($fechaLimiteDB)) {
+                            $strFechaLimite = "Vencido hace " . $diffDays->format("%a") . " dia" . 
+                                ( $diffDays->format("%a") == "1" ? "" : "s");
+                            
+                            $colorStatus = "cor_red_high";
+                        } else {
+                            
+                            if(strtotime(date("Y-m-d")) == strtotime($fechaLimiteDB)) {
+                                $strFechaLimite = "Vence Hoy";
+                                $colorStatus = "cor_red_high";
+                            } else {
+                                
+                                # Dia del vencimiento    --> RED-HIGH
+                                # 1 dia al vencimiento   --> RED-LOW
+                                # 2 dias al vencimiento  --> ORANGE
+                                # 3 dias al vencimiento  --> YELLOW
+                                # 4 o mas al vencimiento --> GREEN
+                                
+                                if($diffDays->format("%a") == "1") {
+                                    $colorStatus = "cor_red_low";
+                                } else if($diffDays->format("%a") == "2") {
+                                    $colorStatus = "cor_orange";
+                                } else if($diffDays->format("%a") == "3") {
+                                    $colorStatus = "cor_yellow";
+                                }
+                                $strFechaLimite = $diffDays->format("%a") . " dias para vencer";    
+                            }
+                        }
+                    }
+
+                    $fechaLimiteDB = str_replace("-", "/", $fechaLimiteDB);
+ 
+                    $row = [
+                        "id"               => $value->id,
+                        "folio"            => $value->folio,
+                        "creador"          => $value->creador,
+                        "persona_dirigida" => $value->persona_dirigida,
+                        "fecha_creacion"   => $value->fecha_creacion,
+                        "dirigido_a"       => $value->dirigido_a,
+                        "depto_dirigido"   => $value->depto_dirigido,
+                        "referencia"       => $value->referencia,
+                        "observaciones"    => $value->observaciones,
+                        "fecha_limite"     => $fechaLimiteDB,
+                        "estatus_limite"   => $strFechaLimite,
+                        "color_status"     => $colorStatus,
+                    ];
+                    array_push($filterCorrespondencia, $row);
+                }
+            
+            
+            if(count($filterCorrespondencia) > 0) {
+                return Response()->json(
+                    array(
+                        'msg'              => "Se obtuvieron los datos correctamente", 
+                        'correspondencias' => $filterCorrespondencia,
+                        'status'           => "success",
+                    )
+                );
+            } else {
+                return Response()->json(
+	                array(
+	                    'msg'			    => 'No se encontraron correspondencias con los criterios de busqueda establecidos', 
+	                    'correspondencias'  => [],
+	                    'status'		    => "error",
+	                )
+	            );
+            }
       
-            return Response()->json(
-                array(
-                    'msg'              => "Se obtuvieron los datos correctamente", 
-                    'correspondencias' => $correspondencias,
-                    'status'           => "success",
-                )
-            );
+            
         } catch(\Illuminate\Database\QueryException $e){
             return Response()->json(
                 array('msg'=>'Error de query al consultar los datos.','error'=>$e)
